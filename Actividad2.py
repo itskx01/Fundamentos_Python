dcc317680f5656feaca8c3d3876319012e96a339
itskx01 <ronalttalarcon@gmail.com>
2026-05-06 07:59:10 -0500

    ComentariosAgregados

--- Actividad2.py
+++ Actividad2.py
@@ -1,20 +1,21 @@
 #Declaracion de variables
 print("\n")
-print("="*8,"Calculadora de Promedio",8*"=")
-nota1 = float(input("Ingrese su nota: "))
-nota2 = float(input("Ingrese su nota: "))
-nota3 = float(input("Ingrese su nota: "))
-nota4 = (nota1+nota2+nota3) / 3 
-nota_maxima = 5.0 - nota4
-aprobado = nota4 >= 3.2
+print("="*8,"Calculadora de Promedio",8*"=") 
+nota1 = float(input("Ingrese su nota: ")) #Ingresa los datos
+nota2 = float(input("Ingrese su nota: ")) #Ingresa los datos
+nota3 = float(input("Ingrese su nota: ")) #Ingresa los datos
+nota4 = (nota1+nota2+nota3) / 3 #Esta haciendo la operacion
+nota_maxima = 5.0 - nota4 #Esta definiendo cuanta nota le falta para llegar a la nota maxima
+aprobado = nota4 >= 3.2 #Esta definiendo que se aprueba desde la nota 3.2
 
-
+# Imprimiendo las variables
 print("\n")
 print("="*8,"Resultado de Calculadora de Notas",8*"=")
-print(f"El promedio es de. {round(nota4, 2)}")
-print(f"Hace falta para la nota maxima: {round(nota_maxima, 2)}")
+print(f"El promedio es de. {round(nota4, 2)}") #Redondea la nota
+print(f"Hace falta para la nota maxima: {round(nota_maxima, 2)}") #Redondea la nota
 print("\n")
 
+# Se esta poniemdo la condicion de aprobado y no aprobado
 if aprobado:
     print("👏👏 "" Aprobaste, felicidades lograste cumplir tu unica responsabilidad "" 👏👏")
 else:
--- Actividad3.py
+++ Actividad3.py
@@ -1,17 +1,18 @@
 # Declarar variables
 print("\n")
 print("="*8,"Calculadora de IMC",8*"=")
-peso = float(input("Ingresa tu peso en kg: "))
-altura = float(input("Ingresa tu altura (Eje. 1.75): "))
-peso_corporal = peso / (altura **2)
+peso = float(input("Ingresa tu peso en kg: ")) #Pide los datos a usar
+altura = float(input("Ingresa tu altura (Eje. 1.75): ")) #Pide los datos a usar
+peso_corporal = peso / (altura **2) #Esta haciendo la operacion 
 
 print("\n")
 print("---Resultado del Analisis---")
 
+#Esta poniendo la condicion de los pesos (bajo peso,normal,sobre peso y obesidad)
 if (peso_corporal  <= 18.5):
     print("Estas bajo de peso ")
-elif 18.5 <= peso_corporal <= 24.9:
-    print("Estas en el peso normal ")
+elif 18.5 <= peso_corporal <= 24.9: #Esta diciendo que es menor o igual a (18.5) y (24.9) es mayor o igual 
+    print("Estas en el peso normal ") #Es lo mismo para todos los demas elif
 elif 25 <= peso_corporal <= 29.9:
-    print("Tienes sobre peso ")
+    print("Tienes sobre peso ") #En este caso es porque a partir de (29.9) ya lo toma como obesidad
 else: print("Tienes obesidad")
