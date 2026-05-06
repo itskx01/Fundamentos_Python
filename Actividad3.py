5d642730fdcb9bfe56068611f7e04ece14444db9
itskx01 <ronalttalarcon@gmail.com>
2026-05-06 08:34:49 -0500

    Correciones

--- Actividad3.py
+++ Actividad3.py
@@ -8,11 +8,15 @@ peso_corporal = peso / (altura **2) #Esta haciendo la operacion
 print("\n")
 print("---Resultado del Analisis---")
 
-#Esta poniendo la condicion de los pesos (bajo peso,normal,sobre peso y obesidad)
-if (peso_corporal  <= 18.5):
+#Esta poniendo la condicion de los pesos (bajo peso,normal,sobre peso y obesidad)    
+if (altura <= 0 or peso <=0 ):
+    print("="*2,"Ingrese un valor en positivo",2*"=")
+elif (peso_corporal  <= 18.5):
     print("Estas bajo de peso ")
 elif 18.5 <= peso_corporal <= 24.9: #Esta diciendo que es menor o igual a (18.5) y (24.9) es mayor o igual 
     print("Estas en el peso normal ") #Es lo mismo para todos los demas elif
 elif 25 <= peso_corporal <= 29.9:
     print("Tienes sobre peso ") #En este caso es porque a partir de (29.9) ya lo toma como obesidad
 else: print("Tienes obesidad")
+
+print(f"\nTu IMC es: {peso_corporal: 2f}")
